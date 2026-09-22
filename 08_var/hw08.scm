acfdbe2c207b6f860c7bd0893e9a6c813c93f008
aiden jiang <aidenj75@nycstudents.net>
2026-09-22 18:23:41 -0400

    Initialize variables and add comments in hw08.scm
    Added comments and defined variables for HW08.

--- 08_var/hw08.scm
+++ 08_var/hw08.scm
@@ -1,1 +1,19 @@
+; AIDEN JIANG
+; Caps Lock
+; Foundations in CS
+; HW08 -- Saving for Posterity
+; 2026-9-17
+; time spent: 0.5hr
+
+
+
+(define a 7) ; should be... 7
+(define b (+ 4 6)) ; should be... 10
+(define c (* 3 5)) ; should be... 15
+(define d (- 20 8)) ; should be... 12
+(define _e (/ 18 3)) ; should be... 6
+
+(+ a b) ; should be... 17
+(* c d) ; should be... 180
+(- (* _e a) b) ; should be... 32
 
